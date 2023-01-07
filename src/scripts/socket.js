76811a65148413ec95a395f8c4e03594dd41a72e
LipezJ <juandavidlipez47@gmail.com>
2023-01-07 18:01:06 -0500

    add socke.io from npm

--- src/scripts/socket.js
+++ src/scripts/socket.js
@@ -1,4 +1,4 @@
-import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
+import { io } from "socket.io-client";
 import { loginSucess, joinSucess, sendSucess } from './main'
 
 const socket = io('http://127.0.0.1:4000', { transports : ['websocket'] });
