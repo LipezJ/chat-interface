3e163b4134ac7b3842b627deb2cbdec37ee88893
LipezJ <juandavidlipez47@gmail.com>
2023-01-08 13:52:33 -0500

    ngrok test

--- src/scripts/socket.js
+++ src/scripts/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { loginSucess, joinSucess, sendSucess } from './main'
 
-const socket = io('http://127.0.0.1:4000', { transports : ['websocket'] });
+const socket = io('https://77d4-190-61-45-156.ngrok.io', { transports : ['websocket'] });
 
 socket.on('alert', data => alert(data.ms))
 socket.on('loginSucess', loginSucess)
